338ed6e8bbf2ee785df7856b4e1cf8ea29bb3be4
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-10-03 08:14:17 -0400

    added on submit attribute to form

--- src/contribute-form.js
+++ src/contribute-form.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 let ContributeForm = () =>
-    <form className="contribute-form">
+    <form onSubmit={ (event) => {
+        event.preventDefault();
+    }}
+        className="contribute-form">
         <input className="contribute-choose-file-button" type="file" />
         <input className="contribute-description-input" type="text" placeholder="description" />
         <input className="contribute-submit-button" type="submit" value="Submit" />
