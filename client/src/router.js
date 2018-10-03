3ef39e9f17471bada6aee265aae3bc1c834cc3dc
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-10-03 13:13:37 -0400

    updated index to render router component

--- client/src/router.js
+++ client/src/router.js
@@ -4,9 +4,11 @@ import NavBar from './nav-bar.js';
 import ContributeScreen from './contribute-screen.js';
 
 let Router = () =>
-    <div className="router">
-        <NavBar/>
-        <Route exact path="/" component={ContributeScreen} />
-    </div>
+    <HashRouter>
+        <div className="router">
+            <NavBar/>
+            <Route exact path="/" component={ContributeScreen} />
+        </div>
+    </HashRouter>
 
 export default Router;
