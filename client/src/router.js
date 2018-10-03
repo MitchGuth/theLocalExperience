ccbeba337de2eee40cb9a47f9c1cfdc3e921a8eb
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-10-03 13:15:12 -0400

    added home screen to router with path

--- client/src/router.js
+++ client/src/router.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import NavBar from './nav-bar.js';
+import HomeScreen from './home-screen';
 import ContributeScreen from './contribute-screen.js';
 
 let Router = () =>
     <HashRouter>
         <div className="router">
             <NavBar/>
-            <Route exact path="/" component={ContributeScreen} />
+            <Route exact path="/" component={HomeScreen} />
+            <Route exact path="/contribute" component={ContributeScreen} />
         </div>
     </HashRouter>
 
