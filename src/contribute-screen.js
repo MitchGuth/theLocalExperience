78144948fc52db0388cee2f2ac6e9a151a22399b
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-10-03 07:34:09 -0400

    created layout for contribute screen

---
+++ src/contribute-screen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from './nav-bar.js';
+import ContributeForm from './contribute-form.js';
+
+let ContributeScreen = () => 
+    <div>
+        <NavBar />
+        <ContributeForm />
+    </div>
+
+export default ContributeScreen;
