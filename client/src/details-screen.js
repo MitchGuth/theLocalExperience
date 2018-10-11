914faaa93cc94730ad4803b5a95b0746174a5bca
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-10-11 13:27:24 -0400

    removed unused component on details screen

--- client/src/details-screen.js
+++ client/src/details-screen.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import DetailRow from './detail-row';
-import MapRow from './map-row.js';
-import DetailsMap from './details-map';
 
 let DetailsScreen = (props) => {
-    return <div>
-        <div className="info-section-details-screen" >
+    return <div className="info-section-details-screen" >
             <DetailRow 
                 experience={props.experiencesArray.find(experience=>
                 (props.match.url === `/details/${experience.postid}`))} 
             />
         </div>
-        <div className="map-section-details-screen">
-            {/* <DetailsMap {...props} /> */}
-        </div>
-    </div>
 }
 
 let ConnectedDetailsScreen = connect(state=> {
