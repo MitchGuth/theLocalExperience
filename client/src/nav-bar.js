c5d5ff7556f1fed2af57bf8786e4d44bb898ddd4
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-10-12 12:01:05 -0400

    added ternary to display link to user contributions only when user is logged in

--- client/src/nav-bar.js
+++ client/src/nav-bar.js
@@ -5,10 +5,15 @@ import { NavLink } from 'react-router-dom';
 let NavBar = () =>
     <div className="nav-bar">
         <NavLink className="nav-bar-link" to="/"> Home </NavLink>
-        <NavLink className="nav-bar-link" to="/login"> Login </NavLink>
-        <NavLink className="nav-bar-link" to="/details"> Details </NavLink>
+        {/* <NavLink className="nav-bar-link" to="/login"> Login </NavLink> */}
+        {/* <NavLink className="nav-bar-link" to="/details"> Details </NavLink> */}
         <NavLink className="nav-bar-link" to="/contribute"> Contribute </NavLink>
-        <NavLink className="nav-bar-link" to="/user/contributions">My Contributions</NavLink>
+        {/* <NavLink className="nav-bar-link" to="/user/contributions">My Contributions</NavLink> */}
+        {(((localStorage.getItem("token"))) ? 
+            <div>
+                <NavLink className="nav-bar-link" to="/user/contributions">My Contributions</NavLink>
+            </div> :
+            <NavLink to="/login"> Login </NavLink>)}
     </div>
 
 export default NavBar;
