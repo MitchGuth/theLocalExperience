f06cee27e2d75e1a2121b2b10c7732e35b5dd110
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-10-15 14:34:02 -0400

    styled nav bar links

--- client/src/nav-bar.js
+++ client/src/nav-bar.js
@@ -8,24 +8,26 @@ let NavBar = (props) =>
         {(props.userName) ? 
             <div className="nav-bar-logged-in-container">
                 <h4 className="nav-bar-welcome-header">Welcome {props.userName}</h4>
-                <NavLink className="nav-bar-link" to="/"> Home </NavLink>
-                <NavLink className="nav-bar-link" to="/contribute"> Contribute </NavLink>
-                <NavLink className="nav-bar-link" to="/user/contributions">My Contributions</NavLink>
-                <button
-                    className="logout-button"
-                    onClick={ (event) => {
-                        event.preventDefault();
-                        localStorage.removeItem('token');
-                        props.dispatch({type: 'LOGOUT_USER' })
-                    }}
-                    >Log Out
-                </button>
+                <div className="nav-bar-links-container">
+                    <NavLink className="nav-bar-link" to="/">Home</NavLink>
+                    <NavLink className="nav-bar-link" to="/contribute">Contribute</NavLink>
+                    <NavLink className="nav-bar-link" to="/user/contributions">My Contributions</NavLink>
+                    <button
+                        className="logout-button nav-bar-link"
+                        onClick={ (event) => {
+                            event.preventDefault();
+                            localStorage.removeItem('token');
+                            props.dispatch({type: 'LOGOUT_USER' })
+                        }}
+                        >Log Out
+                    </button>
+                </div>
             </div>
             :
             <div className="nav-bar-logged-out-container">
-                <NavLink className="nav-bar-link" to="/"> Home </NavLink>
-                <NavLink className="nav-bar-link" to="/contribute"> Contribute </NavLink>
-                <NavLink className="nav-bar-link" to="/login"> Login </NavLink>
+                <NavLink className="nav-bar-link" to="/">Home</NavLink>
+                <NavLink className="nav-bar-link" to="/contribute">Contribute</NavLink>
+                <NavLink className="nav-bar-link" to="/login">Login</NavLink>
             </div>
         }
     </div>
