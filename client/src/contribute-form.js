b8c6a32ed828347f00cf886663d2a2a5f82ee842
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-10-16 08:56:45 -0400

    contribute form input is cleared upon contribution submission

--- client/src/contribute-form.js
+++ client/src/contribute-form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import postContributePhoto from './postContributePhoto';
 import postContribute from './postContribute';
+import clearUserInput from './clearUserInput';
 
 let ContributeForm = (props) =>
     <div className="contribute-container">
@@ -34,6 +35,7 @@ let ContributeForm = (props) =>
                             time: time
                         };
                         postContribute(contributeInformation, props);
+                        clearUserInput(props, 'CONTRIBUTE');
                     })
                 })
             }}
