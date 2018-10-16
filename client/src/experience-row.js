8dc709113c9478183df1696b66f31cba23390875
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-10-16 14:13:17 -0400

    portrait images remain in portrait orientation, adjusted sizing for differen screen sizes

--- client/src/experience-row.js
+++ client/src/experience-row.js
@@ -4,7 +4,9 @@ import { NavLink } from 'react-router-dom';
 let ExperienceRow = (props) => {
     return <li className="experience-row">
         <NavLink className="experience-navlink" to={`/details/${props.experience.postid}`}>
-            <img className="experience-image" src={`${process.env.REACT_APP_API_HOST}` + props.experience.photourl} alt={props.experience.title} />
+            <div className="experience-image-container">
+                <img className="experience-image" src={`${process.env.REACT_APP_API_HOST}` + props.experience.photourl} alt={props.experience.title} />
+            </div>
             <h3 className="experience-title">{props.experience.title}</h3>
         </NavLink>
     </li>
